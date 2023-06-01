59e8fae193d7f8d7d4a3d65a8d99fe04b1575fd5
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2023-06-01 15:44:07 +0100

    Add files via upload

--- isalnum/isalnum.asm
+++ isalnum/isalnum.asm
@@ -1,6 +1,6 @@
 @ Data Section
 .data
-in: .asciz "a"           @ Input string
+in: .asciz " "           @ Input string
 out:  .asciz "Result: %d\n" @ Output string
 
 .global printf  @ Write to console output
@@ -12,45 +12,46 @@ out:  .asciz "Result: %d\n" @ Output string
 
 main:
   LDR  R1, =in      @ Charge string with caracter.
-  LDRB R0, [R1, #0] @ Read byte caracter.
-  BL   isalnum      @ Call islower().
-  MOV  R1, R0       @ Ajustar segundo parâmetro do printf com o valor de retorno.
+  LDRB R2, [R1, #0] @ Read byte caracter.
+  MOV R3, #0
+  BL isadigit
+   BL islower      @ Call islower().
+   BL isupper
+  MOV  R1, R3       @ Ajustar segundo parâmetro do printf com o valor de retorno.
   LDR  R0, =out     @ Carregar string de saída como primeiro parâmetro do printf.
   BL   printf       @ Chamar printf para mostrar o resultado.
-  BL   _exit        @ Pedir ao SO para sair do programa.
+  B   _exit        @ Pedir ao SO para sair do programa.
 
-/*
- * islower(R0)
- * R0 - char
- */
+
 isadigit:
-  CMP   R0, #'0'     @ Check if R0 value is < code char 'a'.
-  MOVLT R0, #0       @ If true is not lower, return 0.
+  PUSH {LR}
+  CMP   R2, #'1'     @ Check if R0 value is < code char 'a'.
+  MOVLT R3, #0       @ If true is not lower, return 0.
   BLT   exit_isalnum @ Exit
-  CMP   R0, #'9'     @ Check if R0 value is > char 'z'.
-  MOVGT R0, #0       @ If true is not lower, return 0.
-  BGT   exit_isalnum @ Exit
-  MOVLE R0, #1       @ 
-  BL islower
+  CMP   R2, #'9'     @ Check if R0 value is > char 'z'.
+  MOVLE R3, #1        @
+  POP {LR}
+  BX LR   
 
 islower:
-  CMP   R0, #'a'     @ Check if R0 value is < code char 'a'.
-  MOVLT R0, #0       @ If true is not lower, return 0.
+  PUSH {LR}
+  CMP   R2, #'a'     @ Check if R0 value is < code char 'a'.
+  # MOVLT R3, #0       @ If true is not lower, return 0.
   BLT   exit_isalnum @ Exit
-  CMP   R0, #'z'     @ Check if R0 value is > char 'z'.
-  MOVGT R0, #0       @ If true is not lower, return 0.
-  BGT   exit_isalnum @ Exit
-  MOVLE R0, #1       @   
-  BL isupper
+  CMP   R2, #'z'
+  MOVLT R3, #1      @ Check if R0 value is > char 'z'.
+  POP {LR} 
+  BX LR
 
 isupper:
-  CMP   R0, #'A'     @ Check if R0 value is < code char 'a'.
-  MOVLT R0, #0       @ If true is not lower, return 0.
+  PUSH {LR}
+  CMP   R2, #'A'     @ Check if R0 value is < code char 'a'.
+  # MOVLT R3, #0       @ If true is not lower, return 0.
   BLT   exit_isalnum @ Exit
-  CMP   R0, #'Z'     @ Check if R0 value is > char 'z'.
-  MOVGT R0, #0       @ If true is not lower, return 0.
-  BGT   exit_isalnum @ Exit
-  MOVLE R0, #1       @   
+  CMP   R2, #'Z'     @ Check if R0 value is > char 'z'.
+  MOVLE R3, #1
+  POP {LR}
+  BX LR   
   
 
 exit_isalnum:
---
+++ isprint/isprint.asm
@@ -0,0 +1,40 @@
+@ Data Section
+.data
+in: .asciz "L"           @ Input string
+out:  .asciz "Result: %d\n" @ Output string
+
+.global printf  @ Write to console output
+.global scanf   @ Read from console input
+
+.section .text
+.global main
+.arm
+
+main:
+  LDR  R1, =in      @ Charge string with caracter.
+  LDRB R0, [R1, #0] @ Read byte caracter.
+  BL   isaprint      @ Call islower().
+  MOV  R1, R0       @ Ajustar segundo parâmetro do printf com o valor de retorno.
+  LDR  R0, =out     @ Carregar string de saída como primeiro parâmetro do printf.
+  BL   printf       @ Chamar printf para mostrar o resultado.
+  B   _exit        @ Pedir ao SO para sair do programa.
+
+/*
+ * islower(R0)
+ * R0 - char
+ */
+isaprint:
+  CMP   R0, #0x20    @ Check if R0 value is < code char '[NULL]'.
+  MOVLT R0, #0      @ If true is not lower, return 0.
+  BLT   exit_islower @ Exit
+  CMP   R0, #0x7E     @ Check if R0 value is > char '[UNIT SEPARATOR]'.
+  MOVGT R0, #0       @ If true is not lower, return 0.
+  BGT   exit_islower @ Exit
+  MOVLE R0, #1       @   
+  
+  exit_islower:
+  BX   LR            @ return;
+    
+_exit:
+  MOV R7, #1         @ Exit syscall
+  SVC 0              @ Invoke
